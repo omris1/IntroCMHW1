d53e41ec89f43662b5d8623a568623f9480ac206
omris1 <omrisass@gmail.com>
2016-03-15 17:47:25 -0400

    testing

--- main.js
+++ main.js
@@ -67,7 +67,7 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "http://connect.facebook.net/en_US/all.js";
+    js.src = "http://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
