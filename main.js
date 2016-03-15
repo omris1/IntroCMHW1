b05f728719dd2fd251e8ed3c71226c356f8b8bd6
omris1 <omrisass@gmail.com>
2016-03-15 18:35:15 -0400

    testing

--- main.js
+++ main.js
@@ -103,8 +103,9 @@ function getLike(){
 
 function getMusic(musicList)
   {
-    console.log(musicList)
+    console.log("called getMusic");
+    console.log(musicList);
     $.get('http://developer.echonest.com/api/v4/artist/suggest?api_key=WIMBATM4FSS9PBEVB&name=Coldplay&results=100', function(responseText) {
-    console.log(responseText);
-  });
+      console.log(responseText);
+    });
 }
